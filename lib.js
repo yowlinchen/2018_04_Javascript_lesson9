4e1e5d2992c792bc8177e036c4976f8af2f70159
yowlinchen <terry.y.chen@intel.com>
2018-05-23 21:24:56 +0800

    using another way to update at the same time

--- lib.js
+++ lib.js
@@ -1,8 +1,25 @@
+ga.lib.BuilletSystem=function(){
+
+};
+
 ga.lib.Plane=function(){
     this.x=ga.ctx.canvas.width/2;
     this.y=ga.ctx.canvas.height/2;
     this.size=20;
     this.update=function(){
+        var speed = 1;
+        if(ga.data.key.left) {
+            this.x-=speed;
+        }
+        if(ga.data.key.top) {
+            this.y-=speed;
+        }
+        if(ga.data.key.right) {
+            this.x+=speed;
+        }
+        if(ga.data.key.bottom) {
+            this.y+=speed;
+        }
         return false;
     };
     this.draw=function(){
